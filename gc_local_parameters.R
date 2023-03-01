fc07416ff9da05bc2c01e8731e7916ffad15b56e
KiyanShabestary <kiyan@kth.se>
2023-03-01 16:23:01 +0000

    Script update
    Updated the script to specific after which time t_min, growth parameters are considered.

--- gc_local_parameters.R
+++ gc_local_parameters.R
@@ -4,13 +4,14 @@
 require(growthcurver)
 require(dplyr)
 
-get_local_growth_params <- function(OD,time,trim_at_time){
+get_local_growth_params <- function(OD,time,trim_at_time,min_t){
   # Function that computes local growth parameters for a given column of a 
   # dataframe. To be used with apply.
   # Input:
   #       - column representing time in hours
   #       - each other column representing the timecourse of a given well, already blanked
-  #       - trim_at_time value in hours
+  #       - trim_at_time value in hours. This is the the upper time value for fitting 
+  #       - min_t in hours. This is the minimal time under which max is not computed
   # Output:
   #       - vector containing local (mu_lin) and exponential (mu_log) growth 
   #         estimates as well as doubling time (from mu_log).
@@ -30,6 +31,9 @@ get_local_growth_params <- function(OD,time,trim_at_time){
   
   # Compute log of the predicted fitted data
   fit$ln_OD_fit <- log(fit$OD_fit) # In R, log function is (logically) the natural logarithm
+  
+  # Only consider timepoints above the treshhold
+  fit <- fit %>% filter(time>min_t)
   
   # Calculate differences between OD and time at each timestep 
   # (note that in R: diff_i <- step_i+1 - step_i)
@@ -51,7 +55,7 @@ d <- growthdata
 
 growth_params <- d %>% 
   select(-time) %>% 
-  apply(MARGIN=2, FUN= get_local_growth_params, d$time, 20) %>%
+  apply(MARGIN=2, FUN= get_local_growth_params, d$time, 20, 5) %>%
   t() %>% as.data.frame() %>% dplyr::rename(max_mu_lin=V1,max_mu_log=V2,min_td=V3)
 growth_params$well <- rownames(growth_params)
 rownames(growth_params)<-NULL
@@ -59,9 +63,12 @@ rownames(growth_params)<-NULL
 
 # For debugging / example on one well
 
+d <- growthdata
+
 OD=d$A1
 time=d$time
 trim_at_time = 20
+min_t = 5
 
 gc_fit <- SummarizeGrowth(data_t = time, 
                           data_n = OD,
@@ -76,6 +83,9 @@ rownames(fit)<-NULL
 # Compute log of the predicted fitted data
 fit$ln_OD_fit <- log(fit$OD_fit) # In R, log function is (logically) the natural logarithm
 
+# Only consider timepoints above the treshhold
+fit <- fit %>% filter(time>min_t)
+
 # Calculate differences between OD and time at each timestep 
 # (note that in R: diff_i <- step_i+1 - step_i)
 local_rates = diff(as.matrix(fit)) %>% as.data.frame()
@@ -83,6 +93,7 @@ local_rates = diff(as.matrix(fit)) %>% as.data.frame()
 local_rates$mu_lin <- local_rates$OD_fit/local_rates$time
 local_rates$mu_log <- local_rates$ln_OD_fit/local_rates$time
 
+# filter out data under t_min
 max_mu_lin <- max(local_rates$mu_lin)
 max_mu_log <- max(local_rates$mu_log)
 
